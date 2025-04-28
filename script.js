92923632c8d4c1a3ff3a1e07644d69a781901736
Rounak Chakraborty <70032763+rounakchakraborty@users.noreply.github.com>
2025-04-28 21:42:54 +0530

    Update script.js
    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// script.js
 function toggleMenu() {
   document.getElementById("navMenu").classList.toggle("show");
+}
+
+function showSection(sectionId) {
+  const sections = document.querySelectorAll("main section");
+  sections.forEach(section => {
+    section.style.display = "none";
+  });
+  document.getElementById(sectionId).style.display = "flex";
 }
