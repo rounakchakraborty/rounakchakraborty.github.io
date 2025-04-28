a6a463b5d82776e4d1ecd07817c220293c7c2440
Rounak Chakraborty <70032763+rounakchakraborty@users.noreply.github.com>
2025-04-28 21:58:57 +0530

    Update script.js
    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function openAbout() {
   document.getElementById("aboutSection").style.display = "block";
   document.getElementById("home").style.display = "none";
 }
+
+// Function to open home section
+function openHome() {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("aboutSection").style.display = "none";
+}
