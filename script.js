7db59b74a26f5c570404532a8dd3e1358333252c
Rounak Chakraborty <70032763+rounakchakraborty@users.noreply.github.com>
2025-04-29 00:45:01 +0530

    Update script.js
    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ function toggleMenu() {
   nav.classList.toggle('show');
 }
 
+function showHome() {
+  document.getElementById('homeSection').classList.remove('hidden');
+  document.getElementById('aboutSection').classList.add('hidden');
+  document.getElementById('contactSection').classList.add('hidden');
+}
+
 function openAbout() {
+  document.getElementById('homeSection').classList.add('hidden');
   document.getElementById('aboutSection').classList.remove('hidden');
   document.getElementById('contactSection').classList.add('hidden');
 }
 
 function openContact() {
+  document.getElementById('homeSection').classList.add('hidden');
   document.getElementById('aboutSection').classList.add('hidden');
   document.getElementById('contactSection').classList.remove('hidden');
 }
